d9ef09cc08ca8c1461714910e45a73577d8adb4f
siddhant <siddhant7847m@gmail.com>
2019-09-16 14:28:17 +0530

    sidebar added

--- default/assets/js/jquery.app.js
+++ default/assets/js/jquery.app.js
@@ -9,10 +9,10 @@
     /* gallery horizontal scroll */
     let mainScroll = 30,
       vgScroll = 30;
-    // let mainBoard = $("#main-board");
-    // mainBoard.mouseover(function() {
-    //   mainScroll = 30;
-    // });
+    let mainBoard = $("#main-board");
+    mainBoard.mouseover(function() {
+      mainScroll = 30;
+    });
 
     $(".gallery-carousel").mousewheel(function(event, delta) {
       mainScroll = 0;
@@ -21,11 +21,11 @@
     });
 
     /* Main board horizontal scroll */
-    // mainBoard.mousewheel(function(event, delta) {
-    //   this.scrollLeft -= delta * mainScroll;
+    mainBoard.mousewheel(function(event, delta) {
+      this.scrollLeft -= delta * mainScroll;
 
-    //   event.preventDefault();
-    // });
+      event.preventDefault();
+    });
 
     /* vsl horizontal scroll */
     $(".video-slider").mousewheel(function(event, delta) {
@@ -155,7 +155,7 @@
       };
 
       var updateScrollPos = function(e, el) {
-        $('.main-board').css("cursor", getCursor());
+        $(".main-board").css("cursor", getCursor());
         var $el = $(el);
         /* Code for scroll vertically */
         // settings.scrollVertical &&
@@ -177,11 +177,24 @@
         },
         mouseup: function() {
           clicked = false;
-          $('.main-board').css("cursor", "auto");
+          $(".main-board").css("cursor", "auto");
         }
       });
     };
     $.dragScroll();
+
+    $("#dismiss, .overlay").on("click", function() {
+      $("#sidebar").removeClass("active");
+      $(".overlay").removeClass("active");
+    });
+
+    $("#sidebarCollapse").on("click", function() {
+      $("#sidebar").addClass("active");
+      $("#sidebar").css("overflow", "visible");
+      $(".overlay").addClass("active");
+      $(".collapse.in").toggleClass("in");
+      $("a[aria-expanded=true]").attr("aria-expanded", "false");
+    });
   });
 
   // init();
