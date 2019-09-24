864592aaa4ff98f4f68984f50922528f1ac97d5a
siddhant <siddhant7847m@gmail.com>
2019-09-24 12:24:29 +0530

    added code in cigna and circle

--- wp-content/themes/dashboard/archive-cigna.php
+++ wp-content/themes/dashboard/archive-cigna.php
@@ -1,1 +1,12 @@
-<h1>This is cigna dashboard</h1>
+<?php
+$args = array('post_type' => 'post');
+$q = new WP_Query($args);
+if ($q->have_posts()) {
+    while ($q->have_posts()) {
+        $q->the_post();
+        //
+        echo the_title() . "<br>";
+
+        //
+    } // end while
+} // end if
